ce7c9eb60f636c7530f805420972ff4d33d4a3ad
Guilherme Hernandez <guilherme@nextar.com.br>
2014-09-21 23:23:53 -0300

    jqyery msonary

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,7 +1,7 @@
 class Pic < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :large => "600x600>",:medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :image, :styles => { :big => "900x900>", :large => "600x600>", :medium => "400x400>", :thumb => "100x100>" }
 	
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
